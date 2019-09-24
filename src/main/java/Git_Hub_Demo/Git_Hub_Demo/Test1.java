465ad2efe815f76f31099ed916d8933ab772d1e2
mdislam77rw <mdislam77rw@gmail.com>
2019-09-24 17:22:59 -0400

    thais is origin framework

---
+++ src/main/java/Git_Hub_Demo/Git_Hub_Demo/Test1.java
@@ -0,0 +1,11 @@
+package Git_Hub_Demo.Git_Hub_Demo;
+
+public class Test1 {
+	static int a = 20;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(a);
+	}
+
+}
